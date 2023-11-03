59b63916763f6a08054e24c7edb3781d8a13c617
lianne <liannealq@hotmail.com>
2023-11-03 16:34:44 +0300

    xbutton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,15 +18,19 @@ const Home = () => {
         {data?.map((book) => {
           return (
             <div className="flex flex-col border-black border-solid border-[2px] m-[10px] w-[200px] h-[100px] p-2 rounded-md">
-              <h1>Title : {book.title} </h1>
-              <h1> Author : {book.author} </h1>
-              <div className="flex flex-row justify-evenly mt-2">
-                <button className=" text-sm rounded-md px-2 border-b-2 border-black border-[1px] bg-red-600 cursor-pointer">
-                  Delete
+              <div className="flex justify-end align-top  ">
+                <button className=" text-[10px] font-semibold hover:bg-red-600 cursor-pointer rounded-full px-1 ">
+                  x
                 </button>
-                <button className=" text-sm rounded-md px-2 border-b-2 border-black border-[1px]  cursor-pointer bg-gray-500 ">
-                  Edit
-                </button>
+              </div>
+              <div>
+                <h1>Title : {book.title} </h1>
+                <h1> Author : {book.author} </h1>
+                <div className="flex justify-end">
+                  <button className=" text-sm rounded-md px-2 border-b-2 border-black border-[1px]  cursor-pointer bg-gray-500 ">
+                    Edit
+                  </button>
+                </div>
               </div>
             </div>
           );
@@ -34,7 +38,7 @@ const Home = () => {
       </div>
       <div className="flex justify-center">
         <h1> Didn't find it? </h1>
-        <button className="  ml-2 text-sm rounded-md px-2 border-b-2 border-black border-[1px] cursor-pointer bg-green-700">
+        <button className="  ml-2 text-sm rounded-md px-2 border-b-2 border-black border-[1px] cursor-pointer  bg-green-400">
           ADD BOOK
         </button>
       </div>
