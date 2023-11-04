1adbc2f038e086ecf888d3c55292aec1b6166f19
lianne <liannealq@hotmail.com>
2023-11-04 20:51:01 +0300

    book item

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,24 @@ import { Link } from "react-router-dom";
 import BookItem from "./component/BookItem";
 
 const Home = () => {
+  const queryClient = useQueryClient();
   const { data, isLoading } = useQuery({
     queryKey: ["books"],
     queryFn: () => getAllBooks(),
   });
+
+  // const { mutate } = useMutation({
+  //   mutationKey: ["book"],
+  //   mutationFn: () => deleteBook(),
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries("books");
+  //   },
+  // });
+
+  // const handledelete = (id) => {
+  //   deleteBook(id);
+  //   queryClient.invalidateQueries("books");
+  // };
 
   // if (isLoading) return <h1> ...loading .. </h1>;
 
@@ -24,9 +38,34 @@ const Home = () => {
           <img src="https://easydrawingguides.com/wp-content/uploads/2020/08/Open-Book-Step-08.png" />
         </div>
         <div className="flex justify-center items-center flex-wrap ">
-          {data?.map((book) => (
-            <BookItem book={book} />
-          ))}
+          {data?.map((book) => {
+            return (
+              <BookItem book={book} />
+
+              // <div className="flex flex-col border-black border-solid border-[2px] m-[10px] w-[200px] h-[100px]  rounded-md bg-sky-300">
+              //   <div className="flex justify-end align-top  ">
+              //     <button
+              //       onClick={() => handledelete(book._id)}
+              //       className=" text-[8px] font-semibold cursor-pointer rounded-full px-1 "
+              //     >
+              //       x
+              //     </button>
+              //   </div>
+              //   <div className="p-2">
+              //     <h1>Title : {book.title} </h1>
+              //     <h1> Author : {book.author} </h1>
+              //     <div className="flex justify-end">
+              //       {/* <Link to={"/Book"}> */}
+              //       {/* <input> </input> */}
+              //       <button className=" text-sm rounded-md px-2 border-b-2 border-black border-[1px]  cursor-pointer hover:bg-gray-300 ">
+              //         Edit
+              //       </button>
+              //       {/* </Link> */}
+              //     </div>
+              //   </div>
+              // </div>
+            );
+          })}
           <div className="flex items-end">
             <h1> Didn't find it? </h1>
             <Link to={"/Book"}>
